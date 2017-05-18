e0d52217ed54cba4e78cb8e66d212c1bef9703b0
Lana Chiad <lanachiad7@gmail.com>
2017-05-18 16:49:34 -0400

    Create post and get requests for questions/:id

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,3 +1,18 @@
 get '/questions/new' do 
+	redirect '/' unless session[:user_id]
 	erb :'questions/_question_form'
+end
+
+post '/questions/:id' do 
+	@question = Question.new(title: params[:title], body: [params[:body]], questioner: current_user.id)
+
+	if @question.save
+		redirect '/questions/:question_id'
+	else
+		status 403
+	end
+end
+
+get '/questions/:id' do
+	"you did it!"
 end
